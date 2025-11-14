694964286c8b527036801ac73b6fe81f491ea400
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-11-15 00:01:37 +0100

    Add logging setup

--- src/__main__.py
+++ src/__main__.py
@@ -16,6 +16,9 @@ try:  # Python 2.7+
     from logging.config import dictConfig
 except ImportError as error:
     raise(f"Unable to import the module needed for logging. Check that your Python version is 2.7 or later.\n{error}")
+logger = logging.getLogger("main")
+
+import yaml
 
 from __init__ import __version__
 
@@ -39,11 +42,37 @@ def argparse_setup() -> argparse.Namespace:
     return parser, args
 
 
-def main():
+def logging_setup(args: argparse.Namespace) -> logging.Logger:
+    """logging_setup Configure the logging system based on the passed arguments.
+
+    This function initializes and adapts the behavior of the main logger based on the options and information provided.
+
+    Arguments:
+        args {argparse.Namespace} -- The parsed command line arguments.
+
+    Returns:
+        logging.Logger -- The configured logger instance.
+    """
+    
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    for handler in logger.handlers:
+        if getattr(handler, "baseFilename", "").endswith("debug.log"):
+            handler.addFilter(lambda record: record.levelno == logging.DEBUG)
+    return logger
+
+
+def initialize():
     parser, args = argparse_setup
     
     with open("./configs/logging.yaml" , "r") as file:
-        config_file = yaml
+        config_file = yaml.safe_load(file)
+        dictConfig(config_file)
+    logger = logging_setup(args)
+
+
+def main():
+    initialize()
     
 
 if __name__ == '__main__':
